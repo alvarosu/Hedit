248a9bcc05021c4f38d57268606652dd43866fcb
elsuga <alvarosuarezdelolmo@gmail.com>
2014-06-10 16:15:30 +0200

    php file

---
+++ hedit.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Hedit
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: Remove style.css from wp_head.
+ * Version: 1.0
+ * Author: alvaro_su
+ * Author URI: https://github.com/alvarosu
+ * License: GPL2
+ */
+
+function remove_stylesheet_theme_styles() {
+    wp_deregister_style( 'twentythirteen-style' );
+    wp_dequeue_style( 'twentythirteen-style' );
+}
+add_action('wp_print_styles', 'remove_stylesheet_theme_styles', 100);
+?>
